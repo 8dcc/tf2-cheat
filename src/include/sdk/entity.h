b1f8af3a94c10a2c63cc2a88684277056003389f
8dcc <8dcc.git@gmail.com>
2023-08-07 14:56:27 +0200

    Add entity_flags enum to entity.h

--- src/include/sdk/entity.h
+++ src/include/sdk/entity.h
@@ -1,6 +1,40 @@
 #ifndef ENTITY_H_
 #define ENTITY_H_
 
+enum entity_flags {
+    FL_ONGROUND              = (1 << 0),
+    FL_DUCKING               = (1 << 1),
+    FL_WATERJUMP             = (1 << 2),
+    FL_ONTRAIN               = (1 << 3),
+    FL_INRAIN                = (1 << 4),
+    FL_FROZEN                = (1 << 5),
+    FL_ATCONTROLS            = (1 << 6),
+    FL_CLIENT                = (1 << 7),
+    FL_FAKECLIENT            = (1 << 8),
+    FL_INWATER               = (1 << 9),
+    FL_FLY                   = (1 << 10),
+    FL_SWIM                  = (1 << 11),
+    FL_CONVEYOR              = (1 << 12),
+    FL_NPC                   = (1 << 13),
+    FL_GODMODE               = (1 << 14),
+    FL_NOTARGET              = (1 << 15),
+    FL_AIMTARGET             = (1 << 16),
+    FL_PARTIALGROUND         = (1 << 17),
+    FL_STATICPROP            = (1 << 18),
+    FL_GRAPHED               = (1 << 19),
+    FL_GRENADE               = (1 << 20),
+    FL_STEPMOVEMENT          = (1 << 21),
+    FL_DONTTOUCH             = (1 << 22),
+    FL_BASEVELOCITY          = (1 << 23),
+    FL_WORLDBRUSH            = (1 << 24),
+    FL_OBJECT                = (1 << 25),
+    FL_KILLME                = (1 << 26),
+    FL_ONFIRE                = (1 << 27),
+    FL_DISSOLVING            = (1 << 28),
+    FL_TRANSRAGDOLL          = (1 << 29),
+    FL_UNBLOCKABLE_BY_PLAYER = (1 << 30)
+};
+
 typedef struct Entity Entity;
 
 typedef struct {
