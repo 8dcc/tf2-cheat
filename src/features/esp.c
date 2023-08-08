346c2b6c9a728776e4e9338674e2f2fd59c637a2
8dcc <8dcc.git@gmail.com>
2023-08-09 01:08:52 +0200

    Fix bounding box esp

--- src/features/esp.c
+++ src/features/esp.c
@@ -3,21 +3,26 @@
 #include "../include/sdk.h"
 #include "../include/globals.h"
 
-#define OUTLINED_BOX(x0, y0, x1, y1, c)                               \
-    METHOD_ARGS(i_surface, SetColor, 0, 0, 0, 0);                     \
-    METHOD_ARGS(i_surface, DrawRect, x0 - 1, y0 - 1, x1 + 1, y1 + 1); \
-    METHOD_ARGS(i_surface, DrawRect, x0 + 1, y0 + 1, x1 - 1, y1 - 1); \
-    METHOD_ARGS(i_surface, SetColor, c.r, c.g, c.b, c.a);             \
-    METHOD_ARGS(i_surface, DrawRect, x0, y0, x1, y1);
+#define OUTLINED_BOX(x, y, w, h, c)                                       \
+    METHOD_ARGS(i_surface, SetColor, 0, 0, 0, 255);                       \
+    METHOD_ARGS(i_surface, DrawRect, x - 1, y - 1, x + w + 2, y + h + 2); \
+    METHOD_ARGS(i_surface, DrawRect, x + 1, y + 1, x + w - 2, y + h - 2); \
+    METHOD_ARGS(i_surface, SetColor, c.r, c.g, c.b, c.a);                 \
+    METHOD_ARGS(i_surface, DrawRect, x, y, x + w, y + h);
 
 static bool get_bbox(Entity* ent, int* x, int* y, int* w, int* h) {
     Collideable* collideable = METHOD(ent, GetCollideable);
     if (!collideable)
         return false;
 
-    vec3_t obb_mins = *METHOD(collideable, ObbMinsPreScaled);
-    vec3_t obb_maxs = *METHOD(collideable, ObbMaxsPreScaled);
-    /* TODO: entity->coordinate_frame() */
+    vec3_t obb_mins = *METHOD(collideable, ObbMins);
+    vec3_t obb_maxs = *METHOD(collideable, ObbMaxs);
+
+    Renderable* renderable = GetRenderable(ent);
+    if (!renderable)
+        return false;
+
+    matrix3x4_t* trans = METHOD(renderable, RenderableToWorldTransform);
 
     vec3_t points[] = { { obb_mins.x, obb_mins.y, obb_mins.z },
                         { obb_mins.x, obb_maxs.y, obb_mins.z },
@@ -29,10 +34,8 @@ static bool get_bbox(Entity* ent, int* x, int* y, int* w, int* h) {
                         { obb_maxs.x, obb_mins.y, obb_maxs.z } };
 
     for (int i = 0; i < 8; i++) {
-        vec3_t t = points[i];
-
-        /* TODO */
-        /* math::transform_vector(points[i], trans, t); */
+        vec3_t t;
+        vec_transform(points[i], trans, &t);
 
         vec2_t s;
         if (!world_to_screen(t, &s))
@@ -95,7 +98,7 @@ void player_esp(void) {
             const rgba_t col = teammate ? (rgba_t){ 10, 240, 10, 255 }
                                         : (rgba_t){ 240, 10, 10, 255 };
 
-            OUTLINED_BOX(x, y, x + w, y + h, col);
+            OUTLINED_BOX(x, y, w, h, col);
         }
 
         /* TODO: Name esp, etc. */
