445bdec79ec9acd90bb5c28e5b90cebd7a49be79
8dcc <8dcc.git@gmail.com>
2023-08-19 18:32:13 +0200

    Change colors using renderview instead of material

--- src/features/chams.c
+++ src/features/chams.c
@@ -13,16 +13,23 @@ static void override_material(bool ignorez, bool wireframe, rgba_t col,
     if (!material)
         return;
 
-    METHOD_ARGS(i_renderview, SetBlend, 1.f);
+    const float_rgba_t fcol = (float_rgba_t){
+        col.r / 255.f,
+        col.g / 255.f,
+        col.b / 255.f,
+        col.a / 255.f,
+    };
 
+    /* Set renderview colors. Will be reset from h_DrawModelExecute later */
+    METHOD_ARGS(i_renderview, SetColorModulation, &fcol);
+    METHOD_ARGS(i_renderview, SetBlend, fcol.a);
+
+    /* Set material flags */
     METHOD_ARGS(material, SetMaterialVarFlag, MATERIAL_VAR_IGNOREZ, ignorez);
     METHOD_ARGS(material, SetMaterialVarFlag, MATERIAL_VAR_WIREFRAME,
                 wireframe);
 
-    METHOD_ARGS(material, AlphaModulate, col.a / 255.f);
-    METHOD_ARGS(material, ColorModulate, col.r / 255.f, col.g / 255.f,
-                col.b / 255.f);
-
+    /* Override material with our own */
     METHOD_ARGS(i_modelrender, ForcedMaterialOverride, material,
                 OVERRIDE_NORMAL);
 }
@@ -33,7 +40,7 @@ bool chams(const ModelRenderInfo_t* pInfo) {
         return CALL_ORIGINAL;
 
     if (strstr(mdl->name, "arms"))
-        override_material(false, false, (rgba_t){ 225, 190, 231, 100 },
+        override_material(false, false, (rgba_t){ 225, 190, 231, 255 },
                           "debug/debugambientcube");
 
     return CALL_ORIGINAL;
