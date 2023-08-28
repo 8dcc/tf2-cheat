6434763568c5ec2db2bede52afeadfb7a08cdcc2
8dcc <8dcc.git@gmail.com>
2023-08-28 17:17:16 +0200

    Add self check to self_unload and FIXME comment

--- src/main.c
+++ src/main.c
@@ -46,7 +46,12 @@ void unload() {
 }
 
 void self_unload(void) {
+    /* FIXME: I think this function always crashes because it is looking for our
+     * lib inside the game's foler. Find a way of adding absolute path here? */
     void* self = dlopen("libenoch.so", RTLD_LAZY | RTLD_NOLOAD);
+    if (!self)
+        return;
+
     dlclose(self); /* Close the call we just made to dlopen() */
     dlclose(self); /* Close the call our injector made */
 }
