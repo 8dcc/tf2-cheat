ec50566c0cebd5d6dd5e8beb4d425b9236469b71
8dcc <8dcc.git@gmail.com>
2023-08-28 20:12:41 +0200

    Store player flags and prediction attributes
    And restore them on finish

--- src/features/prediction.c
+++ src/features/prediction.c
@@ -3,31 +3,40 @@
 #include "../include/sdk.h"
 #include "../include/globals.h"
 
-static float old_curtime   = 0.f;
-static float old_frametime = 0.f;
-static int old_tickcount   = 0;
+static float old_curtime            = 0.f;
+static float old_frametime          = 0.f;
+static int old_tickcount            = 0;
+static int old_flags                = 0;
+static bool old_bInPrediction       = false;
+static bool old_bFirstTimePredicted = false;
 static CMoveData movedata;
 
-/* NOTE: Prediction is a bit of a black box for me. Feel free to make a GitHub
- * issue with more information. */
 void pred_start(usercmd_t* cmd) {
     if (!g.localplayer)
         return;
 
     /* Store for pred_end() */
-    old_curtime   = c_globalvars->curtime;
-    old_frametime = c_globalvars->frametime;
-    old_tickcount = c_globalvars->tickcount;
+    old_curtime             = c_globalvars->curtime;
+    old_frametime           = c_globalvars->frametime;
+    old_tickcount           = c_globalvars->tickcount;
+    old_flags               = g.localplayer->flags;
+    old_bInPrediction       = i_prediction->m_bInPrediction;
+    old_bFirstTimePredicted = i_prediction->m_bFirstTimePredicted;
 
+    /* Get and set random seed */
     cmd->random_seed = MD5_PseudoRandom(cmd->command_number) & 0x7FFFFFFF;
     SetPredictionRandomSeed(cmd);
 
     g.localplayer->m_pCurrentCommand = cmd;
 
+    /* Update global vars */
     const int tick_base     = g.localplayer->nTickBase;
     c_globalvars->curtime   = tick_base * c_globalvars->interval_per_tick;
     c_globalvars->frametime = c_globalvars->interval_per_tick;
     c_globalvars->tickcount = tick_base;
+
+    i_prediction->m_bInPrediction       = true;
+    i_prediction->m_bFirstTimePredicted = false;
 
     /* Updated in h_RunCommand() */
     if (!i_movehelper)
@@ -53,7 +62,10 @@ void pred_end(void) {
 
     g.localplayer->m_pCurrentCommand = NULL;
 
-    c_globalvars->curtime   = old_curtime;
-    c_globalvars->frametime = old_frametime;
-    c_globalvars->tickcount = old_tickcount;
+    c_globalvars->curtime               = old_curtime;
+    c_globalvars->frametime             = old_frametime;
+    c_globalvars->tickcount             = old_tickcount;
+    g.localplayer->flags                = old_flags;
+    i_prediction->m_bInPrediction       = old_bInPrediction;
+    i_prediction->m_bFirstTimePredicted = old_bFirstTimePredicted;
 }
