b34c1e7d1cdd6f16bcc5be3fb6e62bdb002775fb
8dcc <8dcc.git@gmail.com>
2023-11-25 18:52:53 +0100

    Greatelly improve automedigun again, make more legit

--- src/features/aim/medigun.c
+++ src/features/aim/medigun.c
@@ -18,9 +18,9 @@ static bool is_visible(vec3_t start, vec3_t end, Entity* target) {
     return trace.entity == target || trace.fraction > 0.97f;
 }
 
-static vec3_t get_best_center(vec3_t local_shoot_pos) {
+static Entity* get_best_target(vec3_t local_shoot_pos) {
     /* These vars are used to store the best target across iterations */
-    vec3_t best_center          = VEC_ZERO;
+    Entity* best_ent            = NULL;
     float lowest_health_percent = 1.f;
 
     for (int i = 1; i <= g.MaxClients; i++) {
@@ -29,7 +29,7 @@ static vec3_t get_best_center(vec3_t local_shoot_pos) {
         if (!ent || !IsTeammate(ent) || METHOD(ent, GetIndex) == g.localidx)
             continue;
 
-		/* Entity center from collision box */
+        /* Entity center from collision box */
         vec3_t ent_center = GetCenter(ent);
         if (vec_is_zero(ent_center))
             continue;
@@ -48,25 +48,25 @@ static vec3_t get_best_center(vec3_t local_shoot_pos) {
         const float real_max_health = METHOD(ent, GetMaxHealth) * health_mult;
         const float health          = METHOD(ent, GetHealth);
 
-		/* Already at max possible health, ignore */
+        /* Already at max possible health, ignore */
         if (health >= real_max_health)
             continue;
 
-		/* We can't heal him */
+        /* We can't heal him */
         if (!is_visible(local_shoot_pos, ent_center, ent))
             continue;
 
-		/* Health percentage relative to the max health after healing */
+        /* Health percentage relative to the max health after healing */
         const float real_health_percent = health / real_max_health;
 
         /* This teammate has less health than the best one, store */
         if (lowest_health_percent > real_health_percent) {
             lowest_health_percent = real_health_percent;
-            VEC_COPY(best_center, ent_center);
+            best_ent              = ent;
         }
     }
 
-    return best_center;
+    return best_ent;
 }
 
 void automedigun(usercmd_t* cmd) {
@@ -76,42 +76,60 @@ void automedigun(usercmd_t* cmd) {
     if (METHOD(g.localweapon, GetWeaponId) != TF_WEAPON_MEDIGUN)
         return;
 
-    /*--------------------------------------------------------------------*/
+    /* We can't shoot, stop */
+    if (!can_shoot()) {
+        cmd->buttons &= ~IN_ATTACK;
+        return;
+    }
 
-    /* Ticks since last target switch */
-    static int last_release = 0;
+    vec3_t local_shoot_pos = METHOD(g.localplayer, GetShootPos);
+
+    /* Get best teammate, depending on health percentage */
+    Entity* best_target = get_best_target(local_shoot_pos);
+
+    /* No valid target */
+    if (!best_target) {
+        /* Even if there is no good target, if we are healing, don't stop */
+        if (IsMedigunHealing(g.localweapon))
+            cmd->buttons |= IN_ATTACK;
+
+        return;
+    }
+
+    /* Ticks since we switched target */
+    static uint32_t ticks_since_switch = 0;
+
+    /* Every tick, increase count */
+    ticks_since_switch++;
 
     /* TODO: Setting */
     const int switch_time = 30;
 
-    /* A tick has passed since last release */
-    last_release++;
+    /* If we are already healing, and enough ticks have passed since last switch
+     * depending on user setting. */
+    if (IsMedigunHealing(g.localweapon) && ticks_since_switch >= switch_time) {
+        CBaseHandle healed_handler = GetMedigunHealingHandler(g.localweapon);
+        const int healed_idx       = CBaseHandle_GetEntryIndex(healed_handler);
+        if (healed_idx < 1 || healed_idx >= g.MaxClients)
+            return;
 
-    if (!can_shoot() || last_release >= switch_time) {
-        /* Every `switch_time` seconds, release attack for 1 tick */
-        cmd->buttons &= ~IN_ATTACK;
+        /* If it's already the best possible target, hold it. Otherwise, release
+         * attack so we can find the best target on the next tick */
+        if (METHOD(best_target, GetIndex) == healed_idx) {
+            cmd->buttons |= IN_ATTACK;
+        } else {
+            cmd->buttons &= ~IN_ATTACK;
 
-        /* Store we just did that */
-        last_release = 0;
+            /* Store we switched on this tick */
+            ticks_since_switch = 0;
+        }
+
+        /* Always return if we are already healing */
         return;
     }
 
-    /* If we are already healing, hold it */
-    if (IsMedigunHealing(g.localweapon)) {
-        cmd->buttons |= IN_ATTACK;
-        return;
-    }
-
-    /*--------------------------------------------------------------------*/
-
-    vec3_t local_shoot_pos = METHOD(g.localplayer, GetShootPos);
-
-	/* Get center of best teammate, depending on health percentage */
-    vec3_t target_pos = get_best_center(local_shoot_pos);
-
-    /* No valid target */
-    if (vec_is_zero(target_pos))
-        return;
+    /* Get the entity center from collision box */
+    vec3_t target_pos = GetCenter(best_target);
 
     /* Get target angle */
     vec3_t target_angle = vec_to_ang(vec_sub(target_pos, local_shoot_pos));
