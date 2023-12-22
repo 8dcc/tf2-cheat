2ece089692903a1d0783f5a82fb0587b226c53ce
nepcat <153364073+nepcat@users.noreply.github.com>
2023-12-23 01:56:45 +0300

    Add reloading

--- inject.sh
+++ inject.sh
@@ -29,6 +29,12 @@ if [[ $cozettevector -lt 1 ]]; then
     exit 1
 fi
 
+old_tmp_libpath_txt="/tmp/hl2_linux_${pid}_enoch.txt"
+old_tmp_libpath=$(head -n 1 "$old_tmp_libpath_txt")
+if [[ -z "$old_tmp_libpath" && "$old_tmp_libpath" != "" ]]; then
+    echo "inject.sh: old_tmp_libpath: $old_tmp_libpath"
+fi
+
 tmp_libpath=""
 if [[ "$1" == "unload" ]] && [[ ! -z "$2" ]]; then
     # While unloading use the right library path
@@ -40,11 +46,12 @@ else
     tmp_libpath+=".so"
     cp -p "$libpath" "$tmp_libpath"
 fi
-echo "Our cheat library is located at $tmp_libpath"
+echo "inject.sh: Our cheat library is located at $tmp_libpath"
 
 # Used to echo each command. For debugging.
 #set -x
 if [[ "$1" == "unload" ]] && [[ ! -z "$tmp_libpath" ]]; then
+    # Unloading
     gdb -n -q -batch                                        \
          -ex "attach $pid"                                  \
          -ex "set \$dlopen = (void* (*)(char*, int))dlopen" \
@@ -58,7 +65,9 @@ if [[ "$1" == "unload" ]] && [[ ! -z "$tmp_libpath" ]]; then
          -ex "call \$dlerror()"                             \
          -ex "detach"                                       \
          -ex "quit"
+    rm "$old_tmp_libpath_txt"
 elif [[ "$1" == "debug" ]]; then
+   # Injecting in debug mode
    echo "Launching in debug mode."
    gdb -n -q                                               \
         -ex "attach $pid"                                  \
@@ -70,21 +79,46 @@ elif [[ "$1" == "debug" ]]; then
         -ex "continue" # Comment this line for manual debug
 
     set +x
-    echo -e "\nDone. Unload cheat using:\n$0 unload $tmp_libpath"
+    echo -e "\nEnoch loaded. Unload cheat using:\n$0 unload $tmp_libpath"
     rm "$tmp_libpath"
+    echo "$tmp_libpath" > "$old_tmp_libpath_txt"
 else
-    # Im still thinking of a way to reload cheat while using temp library
-    # so i'll remove it for now.
-    gdb -n -q -batch                                       \
-        -ex "attach $pid"                                  \
-        -ex "set \$dlopen = (void* (*)(char*, int))dlopen" \
-        -ex "set \$dlclose = (int (*)(void*))dlclose"      \
-        -ex "set \$dlerror =  (char* (*)(void))dlerror"    \
-        -ex "call \$dlopen(\"$tmp_libpath\", 2)"           \
-        -ex "call \$dlerror()"                             \
-        -ex "detach"                                       \
-        -ex "quit"
+    if [[ ! -z "$old_tmp_libpath" ]] && [[ "$old_tmp_libpath" != "" ]] && grep -q "$old_tmp_libpath" "/proc/$pid/maps"; then
+        # Reloading
+        echo -e "Enoch already loaded. Reloading...\n";
+
+        # 0x2 -> RTLD_NOW
+        # 0x6 -> RTLD_LAZY | RTLD_NOLOAD
+        # For more info on the 3 mid lines, see self_unload() in main.c
+        gdb -n -q -batch                                         \
+            -ex "attach $pid"                                    \
+            -ex "set \$dlopen = (void* (*)(char*, int))dlopen"   \
+            -ex "set \$dlclose = (int (*)(void*))dlclose"        \
+            -ex "set \$dlerror =  (char* (*)(void))dlerror"      \
+                                                                 \
+            -ex "set \$self = \$dlopen(\"$old_tmp_libpath\", 6)" \
+            -ex "call \$dlclose(\$self)"                         \
+            -ex "call \$dlclose(\$self)"                         \
+                                                                 \
+            -ex "call \$dlopen(\"$tmp_libpath\", 2)"             \
+            -ex "call \$dlerror()"                               \
+            -ex "detach"                                         \
+            -ex "quit"
+    else
+        # Injecting
+        gdb -n -q -batch                                       \
+            -ex "attach $pid"                                  \
+            -ex "set \$dlopen = (void* (*)(char*, int))dlopen" \
+            -ex "set \$dlclose = (int (*)(void*))dlclose"      \
+            -ex "set \$dlerror =  (char* (*)(void))dlerror"    \
+            -ex "call \$dlopen(\"$tmp_libpath\", 2)"           \
+            -ex "call \$dlerror()"                             \
+            -ex "detach"                                       \
+            -ex "quit"
+    fi
+
     set +x
-    echo -e "\nDone. Unload cheat using:\n$0 unload $tmp_libpath"
+    echo -e "\nEnoch loaded. Unload cheat using:\n$0 unload $tmp_libpath"
     rm "$tmp_libpath"
+    echo "$tmp_libpath" > "$old_tmp_libpath_txt"
 fi
