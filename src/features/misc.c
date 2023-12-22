a3f8bdaefd4e12de332f6b1f48327a275deeb15a
8dcc <8dcc.git@gmail.com>
2023-12-23 00:18:41 +0100

    Change sticky spawn cache method

--- src/features/misc.c
+++ src/features/misc.c
@@ -69,11 +69,12 @@ void autobackstab(usercmd_t* cmd) {
 }
 
 /*----------------------------------------------------------------------------*/
+// TODO: Move to separate file
 
-#define CLEAR_STICKIES_AND_RETURN() \
-    {                               \
-        last_sticky = -1;           \
-        return;                     \
+#define CLEAR_SPAWN_CACHE_AND_RETURN() \
+    {                                  \
+        spawn_cache_pos = 0;           \
+        return;                        \
     }
 
 #define MAX_STICKIES 8
@@ -83,57 +84,78 @@ typedef struct {
     float spawn_time;
 } SpawnData;
 
-static SpawnData stickies[MAX_STICKIES];
-static int last_sticky = -1;
+static SpawnData spawn_cache[MAX_STICKIES];
+static int spawn_cache_pos = 0;
 
-static bool can_detonate(int ent_idx, int item_def_index) {
+static inline float get_arm_time(int item_def_index) {
+    return (item_def_index == Demoman_s_TheQuickiebombLauncher) ? 0.6f : 0.8f;
+}
+
+static void spawn_cache_update(int* indexes, int sz) {
+    /* We use this method to clear all the unused stickies in the cache, and
+     * only keep the real ones from indexes[] */
+    SpawnData new_cache[MAX_STICKIES];
+    int new_cache_pos = 0;
+
+    /* Clear new cache */
+    for (int i = 0; i < MAX_STICKIES; i++) {
+        new_cache[i].idx        = 0;
+        new_cache[i].spawn_time = 0.f;
+    }
+
+    /* Iterate input indexes */
+    for (int i = 0; i < sz && new_cache_pos < MAX_STICKIES; i++) {
+        /* Is it already stored in the spawn cache? */
+        float spawn_time = 0.f;
+        for (int j = 0; j < spawn_cache_pos; j++) {
+            if (indexes[i] == spawn_cache[j].idx) {
+                spawn_time = spawn_cache[j].spawn_time;
+                break;
+            }
+        }
+
+        new_cache[new_cache_pos].idx = indexes[i];
+
+        /* If this sticky was already in the spawn cache, use that spawn time.
+         * Otherwise, store it spawned now */
+        new_cache[new_cache_pos].spawn_time =
+          (spawn_time > 0.f) ? spawn_time : c_globalvars->curtime;
+
+        new_cache_pos++;
+    }
+
+    memcpy(spawn_cache, new_cache, sizeof(new_cache));
+    spawn_cache_pos = new_cache_pos;
+}
+
+static bool spawn_cache_can_detonate(int ent_idx, int item_def_index) {
     float spawn_time = 0.f;
 
     /* Check if we saved the spawn time of this sticky */
-    for (int i = 0; i <= last_sticky; i++) {
-        if (stickies[i].idx == ent_idx) {
-            spawn_time = stickies[i].spawn_time;
+    for (int i = 0; i < spawn_cache_pos; i++) {
+        if (spawn_cache[i].idx == ent_idx) {
+            spawn_time = spawn_cache[i].spawn_time;
             break;
         }
     }
 
     /* Did not find spawn time for sticky, store it just spawned */
-    if (spawn_time == 0.f) {
-        if (last_sticky < MAX_STICKIES - 1) {
-            stickies[++last_sticky] = (SpawnData){
-                .idx        = ent_idx,
-                .spawn_time = c_globalvars->curtime,
-            };
-        }
+    if (spawn_time == 0.f)
         return false;
-    }
 
     /* Return true if N seconds have passed since spawn time */
-    const float arm_time =
-      (item_def_index == Demoman_s_TheQuickiebombLauncher) ? 0.6f : 0.8f;
+    const float arm_time = get_arm_time(item_def_index);
     return c_globalvars->curtime >= spawn_time + arm_time;
-}
-
-static void clear_detonated_stickies(int item_def_index) {
-    const float arm_time =
-      (item_def_index == Demoman_s_TheQuickiebombLauncher) ? 0.6f : 0.8f;
-
-    /* New position in the `stickies' array for the non-armed stickies */
-    int new_last = -1;
-
-    /* Iterate array, only save the stickies that are not fully armed */
-    for (int i = 0; i <= last_sticky; i++)
-        if (c_globalvars->curtime < stickies[i].spawn_time + arm_time)
-            stickies[++new_last] = stickies[i];
-
-    /* Update number of stickies in array */
-    last_sticky = new_last;
 }
 
 void auto_detonate_stickies(usercmd_t* cmd) {
     if (!settings.auto_detonate || !g.localplayer || !g.localweapon ||
         !g.IsAlive)
-        CLEAR_STICKIES_AND_RETURN();
+        CLEAR_SPAWN_CACHE_AND_RETURN();
+
+    /* User is manually detonating */
+    if (cmd->buttons & IN_ATTACK2)
+        return;
 
     /* Get weapon in second slot. We have to iterate because the index in
      * `m_hMyWeapons' is not always one (if we use boots, for example). We can't
@@ -146,7 +168,7 @@ void auto_detonate_stickies(usercmd_t* cmd) {
         Weapon* cur_weapon =
           (Weapon*)METHOD_ARGS(i_entitylist, GetClientEntity, idx);
         if (!cur_weapon)
-            CLEAR_STICKIES_AND_RETURN();
+            CLEAR_SPAWN_CACHE_AND_RETURN();
 
         /* Check if we have launcher in second slot */
         const int cur_weapon_id = METHOD(cur_weapon, GetWeaponId);
@@ -158,10 +180,13 @@ void auto_detonate_stickies(usercmd_t* cmd) {
 
     /* We didn't find a valid TF_WEAPON_PIPEBOMBLAUNCHER in our weapons */
     if (!secondary)
-        CLEAR_STICKIES_AND_RETURN();
+        CLEAR_SPAWN_CACHE_AND_RETURN();
 
     /* Used in previous 2 functions to check the arm time of the stickies */
     const int wpn_item_def_idx = secondary->m_iItemDefinitionIndex;
+
+    int sticky_indexes[MAX_STICKIES] = { 0 };
+    int sticky_indexes_pos           = 0;
 
     /* Iterate entities, searching for stickies */
     for (int i = g.MaxClients + 1; i < g.MaxEntities; i++) {
@@ -188,9 +213,32 @@ void auto_detonate_stickies(usercmd_t* cmd) {
         if (thrower_idx != g.localidx)
             continue;
 
+        /* We don't have space left in the array */
+        if (sticky_indexes_pos >= MAX_STICKIES)
+            break;
+
+        /* One of our stickies, store */
+        sticky_indexes[sticky_indexes_pos++] = i;
+    }
+
+    /* No stickies, clear spawn cache and return */
+    if (sticky_indexes_pos == 0)
+        CLEAR_SPAWN_CACHE_AND_RETURN();
+
+    /* Update spawn cache with our sticky list. If some stickies are not in the
+     * spawn cache, it will add them. */
+    spawn_cache_update(sticky_indexes, sticky_indexes_pos);
+
+    /* Iterate our stickies again */
+    for (int i = 0; i < sticky_indexes_pos; i++) {
+        const int ent_idx = sticky_indexes[i];
+
         /* Check if the sticky is armed */
-        if (!can_detonate(i, wpn_item_def_idx))
+        if (!spawn_cache_can_detonate(ent_idx, wpn_item_def_idx))
             continue;
+
+        /* We already validated the entity when adding it to the array */
+        Entity* sticky = g.ents[ent_idx];
 
         /* Valid sticky. Calculate position once. */
         const vec3_t sticky_pos = *METHOD(sticky, WorldSpaceCenter);
@@ -225,14 +273,10 @@ void auto_detonate_stickies(usercmd_t* cmd) {
             /* Is there anything between the sticky and the target? */
             if (is_enemy_visible(sticky_pos, player_pos, player)) {
                 cmd->buttons |= IN_ATTACK2;
-                clear_detonated_stickies(wpn_item_def_idx);
                 return;
             }
         }
     }
-
-    if (cmd->buttons & IN_ATTACK2)
-        clear_detonated_stickies(wpn_item_def_idx);
 }
 
 /*----------------------------------------------------------------------------*/
