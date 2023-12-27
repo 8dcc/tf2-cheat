3a4efa7a5de5c6e1a252083ffd1e1f1ded72bfde
8dcc <8dcc.git@gmail.com>
2023-12-27 21:58:30 +0100

    Update playerlist.c with new names

--- src/playerlist.c
+++ src/playerlist.c
@@ -1,40 +1,38 @@
-#include "features.h"
-#include "../include/playerlist.h"
-#include "../include/sdk.h"
-#include "../include/globals.h"
-#include <stdio.h>
 
-void playerlist_print_all(usercmd_t* cmd) {
+#include <stdio.h>
+#include "include/playerlist.h"
+#include "include/sdk.h"
+#include "include/globals.h"
+
+void plist_print_all(usercmd_t* cmd) {
     for (int i = 0; i < g.MaxClients; i++) {
-        player_list_player_t* playerlist_player = &g.playerlist_players[i];
+        plist_player_t* player = &g.playerlist[i];
+
         /* Check if player is valid before printing him */
-        if (!playerlist_player || !playerlist_player->is_good ||
-            playerlist_player->player_info.ishltv) {
+        if (!player || !player->is_good || player->pinfo.ishltv)
             continue;
-        }
-        printf("PlayerList BEGIN: (TICK %d)", cmd->tick_count);
-        printf("Player %d, name %s, userid: %d, preset: %d, target "
-               "ignored: %s, is a bot: %s\n",
-               i, playerlist_player->player_info.name,
-               playerlist_player->player_info.userID, playerlist_player->preset,
-               playerlist_player->should_be_ignored ? "true" : "false",
-               playerlist_player->player_info.fakeplayer ? "true" : "false");
-        printf("PlayerList END: (TICK %d)", cmd->tick_count);
+
+        printf("PlayerList BEGIN: (TICK %d)\n", cmd->tick_count);
+        printf(
+          "[%d] Name %s | UserID: %d | Preset: %d | Ignored: %d | Is bot: %d\n",
+          i, player->pinfo.name, player->pinfo.userID, player->preset,
+          player->is_ignored, player->pinfo.fakeplayer);
+        printf("PlayerList END: (TICK %d)\n\n", cmd->tick_count);
     }
 }
 
-const char* player_list_preset_to_const_char(
-  const enum PLAYER_LIST_PLAYER_PRESET preset) {
+const char* plist_preset_to_str(enum EPlayerListPlayerPreset preset) {
     switch (preset) {
+        default:
         case UNSET:
-            return PLAYER_LIST_PRESET_CCHAR_UNSET;
+            return PLIST_PRESET_STR_UNSET;
         case FRIEND:
-            return PLAYER_LIST_PRESET_CCHAR_FRIEND;
+            return PLIST_PRESET_STR_FRIEND;
         case SOFT_RAGE:
-            return PLAYER_LIST_PRESET_CCHAR_SOFT_RAGE;
+            return PLIST_PRESET_STR_SOFT_RAGE;
         case RAGE:
-            return PLAYER_LIST_PRESET_CCHAR_RAGE;
+            return PLIST_PRESET_STR_RAGE;
         case MAX_RAGE:
-            return PLAYER_LIST_PRESET_CCHAR_MAX_RAGE;
+            return PLIST_PRESET_STR_MAX_RAGE;
     }
 }
